e6b1e268f91c62f8c6ef1636200296354d418a1b
kanakanho <kahoshibakichi@gmail.com>
2024-06-06 15:51:16 +0900

    add: three.jsの中でhtml要素を表示する

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+'use client';
+
+import { Html } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { ARButton } from '@react-three/xr';
+import { Message } from './Message';
+
 export default function Home() {
-  return <main />;
+  return (
+    <>
+      <ARButton />
+      <Canvas style={{ width: '100vw', height: '100vh' }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Html position={[-3, 3, 0]}>
+          <Message />
+        </Html>
+      </Canvas>
+    </>
+  );
 }
